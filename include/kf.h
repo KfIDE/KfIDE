616843788834d9083f236a8baa1f7a1101f12dd7
ps4star <hoenn4life@gmail.com>
2023-04-15 14:51:39 -0400

    resolve conflicts

--- include/kf.h
+++ include/kf.h
@@ -3,19 +3,29 @@
 
 #include <stdlib.h>
 
-
-/* Platform */
-typedef void *PlatformSpecificContext;
-
+#include <GL/gl.h>
+#include <EGL/egl.h>
 
 typedef struct {
 	isize x, y, w, h;
 } IRect;
 
+/* Platform */
+typedef void *PlatformSpecificContext;
+
+typedef struct {
+	isize x, y, w, h;
+} IRect;
+
+PlatformSpecificContext kf_get_platform_specific_context(void);
+void kf_init_video(PlatformSpecificContext ctx, gbString title, isize x, isize y, isize w, isize h, bool maximized);
+
+void kf_resize_window(PlatformSpecificContext ctx, isize w, isize h);
+
 typedef struct {
 	isize mouse_x, mouse_y, mouse_xrel, mouse_yrel;
+	bool exited;
 } EventState;
-
 
 /* Returns platform specific variables as a struct. */
 PlatformSpecificContext kf_get_platform_specific_context(void);
