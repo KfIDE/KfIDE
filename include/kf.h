abb3c01bec059ad20f6f7723acc6572c25a7f2de
SaCode <69605234+EimaMei@users.noreply.github.com>
2023-04-16 11:52:43 +0300

    kf_terminate_video + kf_flush_buffer, opengl test function and some small changes

--- include/kf.h
+++ include/kf.h
@@ -5,7 +5,7 @@
 
 #ifdef __APPLE__
 	#define GL_SILENCE_DEPRECATION
-	#include <OpenGL/OpenGL.h>
+	#include <OpenGL/gl.h>
 #else
 	#include <GL/gl.h>
 	#include <EGL/egl.h>
@@ -31,8 +31,13 @@ PlatformSpecificContext kf_get_platform_specific_context(void);
 
 /* Initializes a window and an OpenGL instance. */
 void kf_init_video(PlatformSpecificContext ctx, gbString title, isize x, isize y, isize w, isize h, bool maximized);
+/* Frees the window and OpenGL instance. */
+void kf_terminate_video(PlatformSpecificContext ctx);
 /* Resizes the window. */
 void kf_resize_window(PlatformSpecificContext ctx, isize w, isize h);
 
 /* Grabs events (non-blocking/non-vsync) and sets EventState accordingly */
 void kf_analyze_events(PlatformSpecificContext ctx, EventState *out);
+
+/* Flushes everything drawn with OpenGL to the buffer. */
+void kf_flush_buffer(PlatformSpecificContext ctx);
--- src/kf.c
+++ src/kf.c
@@ -14,6 +14,22 @@ typedef struct {
 static GlobalVars g;
 
 
+/* To test if OpenGL is properly initialized. */
+void opengl_test()
+{
+	glClearColor(0.1, 0.1f, 0.1f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT); /* Not sure if this should be a function on its own, so I'll just leave it here (Based on https://www.khronos.org/opengl/wiki/Common_Mistakes#Swap_Buffers). */
+
+	glBegin(GL_TRIANGLES);
+		glColor3f(1, 0, 0); glVertex2f(-1.0f, -1.0f);
+		glColor3f(0, 1, 0); glVertex2f( 1.0f, -1.0f);
+		glColor3f(0, 0, 1); glVertex2f( 0.0f,  1.0f);
+	glEnd();
+
+	kf_flush_buffer(g.platform_context);
+}
+
+
 int main(int argc, char **argv)
 {
 	g.platform_context = kf_get_platform_specific_context();
@@ -26,7 +42,10 @@ int main(int argc, char **argv)
 		if (e->exited) {
 			break;
 		}
+
+		opengl_test();
 	}
+	kf_terminate_video(g.platform_context);
 
 	return 0;
 }
